fc161f7c73581e67a679573d63116a4c043a86c5
iamsahu <praffulsahu@gmail.com>
2022-06-06 17:41:03 +0530

    feat: doc only mode

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,117 +1,109 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Yield Protocol',
-  tagline: 'Fixedrates are cool',
-  url: 'https://iamsahu.github.io',
-  baseUrl: '/yield-docs/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+	title: "Yield Protocol",
+	tagline: "Fixedrates are cool",
+	url: "https://iamsahu.github.io",
+	baseUrl: "/yield-docs/",
+	onBrokenLinks: "throw",
+	onBrokenMarkdownLinks: "warn",
+	favicon: "img/favicon.ico",
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'iamsahu', // Usually your GitHub org/user name.
-  projectName: 'yield-docs', // Usually your repo name.
+	// GitHub pages deployment config.
+	// If you aren't using GitHub pages, you don't need these.
+	organizationName: "iamsahu", // Usually your GitHub org/user name.
+	projectName: "yield-docs", // Usually your repo name.
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+	// Even if you don't use internalization, you can use this field to set useful
+	// metadata like html lang. For example, if your site is Chinese, you may want
+	// to replace "en" with "zh-Hans".
+	i18n: {
+		defaultLocale: "en",
+		locales: ["en"],
+	},
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/iamsahu/yield-docs/tree/master/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
-    ],
-  ],
+	presets: [
+		[
+			"classic",
+			/** @type {import('@docusaurus/preset-classic').Options} */
+			({
+				docs: {
+					routeBasePath: "/",
+					sidebarPath: require.resolve("./sidebars.js"),
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					editUrl: "https://github.com/iamsahu/yield-docs/tree/master/",
+				},
+				theme: {
+					customCss: require.resolve("./src/css/custom.css"),
+				},
+			}),
+		],
+	],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'YieldProtocol',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type: 'doc',
-            docId: 'Cauldron',
-            position: 'left',
-            label: 'Documentation',
-          },
-          
-          {
-            href: 'https://github.com/yieldprotocol',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Docs',
-                to: '/docs/Cauldron',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Discord',
-                href: 'https://discord.gg/JAFfDj5',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/yieldprotocol',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'GitHub',
-                href: 'https://github.com/yieldprotocol',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} YieldProtocol, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+	themeConfig:
+		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+		({
+			navbar: {
+				title: "YieldProtocol",
+				logo: {
+					alt: "My Site Logo",
+					src: "img/logo.svg",
+				},
+				items: [
+					{
+						type: "doc",
+						docId: "Cauldron",
+						position: "left",
+						label: "Documentation",
+					},
+
+					{
+						href: "https://github.com/yieldprotocol",
+						label: "GitHub",
+						position: "right",
+					},
+				],
+			},
+			footer: {
+				style: "dark",
+				links: [
+					
+					{
+						title: "Community",
+						items: [
+							{
+								label: "Discord",
+								href: "https://discord.gg/JAFfDj5",
+							},
+							{
+								label: "Twitter",
+								href: "https://twitter.com/yieldprotocol",
+							},
+						],
+					},
+					{
+						title: "More",
+						items: [
+							{
+								label: "GitHub",
+								href: "https://github.com/yieldprotocol",
+							},
+						],
+					},
+				],
+				copyright: `Copyright © ${new Date().getFullYear()} YieldProtocol, Inc. Built with Docusaurus.`,
+			},
+			prism: {
+				theme: lightCodeTheme,
+				darkTheme: darkCodeTheme,
+			},
+		}),
 };
 
 module.exports = config;
